c1814e479e00f4f6929f36b26c741e8ba9897c74
captainJack33 <qfhjack@gmail.com>
2020-11-05 22:28:33 -0800

    small enhance

---
+++ ShoppingSite/App/doc/Note.md
@@ -0,0 +1,12 @@
+#AAA
+
+## User
+- 用户表设计
+    - username
+    - password
+    - email
+    - phone
+    - icon
+    - is_active
+    - is_delete
+    
--- ShoppingSite/App/models.py
+++ ShoppingSite/App/models.py
@@ -100,3 +100,15 @@ class  Goods(models.Model):
     productnum = models.IntegerField(default=1)
     class  Meta:
         db_table = 'aaa_goods'
+
+
+class AAAUser(models.Model):
+    u_username = models.CharField(max_length=32, unique=True)
+    u_password = models.CharField(max_length=256)
+    u_email = models.CharField(max_length=64, unique=True)
+    u_icon = models.ImageField(upload_to='icons/%Y/%m/%d/')
+    is_active = models.BooleanField(default=False)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'aaa_user'
